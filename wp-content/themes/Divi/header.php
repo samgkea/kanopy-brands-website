1a1f24618294bc06d79898a128b66491a56e801b
samgkea <samg@keaadvertising.com>
2019-04-11 15:33:51 -0400

    Added analytics, uploaded compressed images

--- wp-content/themes/Divi/header.php
+++ wp-content/themes/Divi/header.php
@@ -25,6 +25,14 @@
 	</script>
 
 	<?php wp_head(); ?>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-130515432-3"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'UA-130515432-3');
+	</script>
 </head>
 <body <?php body_class(); ?>>
 <?php
